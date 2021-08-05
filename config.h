b870249e909ce3d7fd7ad0654f86ec3d084eb098
gerardet46 <gerrysoft@outlook.es>
2021-08-05 16:32:29 +0200

    afegit per obrir un terminal amb sa mateixa carpeta

--- config.h
+++ config.h
@@ -237,6 +237,7 @@ static Shortcut shortcuts[] = {
 	{ MODKEY,               XK_l,           externalpipe,   {.v = openurlcmd } },
 	{ MODKEY,               XK_y,           externalpipe,   {.v = copyurlcmd } },
 	{ MODKEY,               XK_o,           externalpipe,   {.v = copyoutput } },
+	{ TERMMOD,              XK_Return,      newterm,        {.i =  0} },
 };
 
 /*
